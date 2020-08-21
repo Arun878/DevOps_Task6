5d3058a696935780a8974c7d279a8db228700b5b
Arun Kumar <arunsumit878@gmail.com>
2020-08-21 12:45:33 +0530

    Test3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    prinddt("<h1>Hello World !!</h1>");
+    prinddt("<h1>Hello World !!!</h1>");
     print("<h2>Image</h2>");
     print("<img src='webdata/im.jpg' width=600 height=600>");
 
