ee65a069056ee7d1df9d66bc3bee1b217c9e6c3c
Arun Kumar <arunsumit878@gmail.com>
2020-08-21 12:29:07 +0530

    Commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    prinddt("<h1>Hello</h1>");
+    print("<h2>Image</h2>");
+    print("<img src='webdata/im.jpg' width=600 height=600>");
+
+?>
---
+++ myt6.groovy
@@ -0,0 +1,89 @@
+job("Task-6-Job-2"){
+    steps{
+        shell('''
+                cd /workspace
+
+                if kubectl get ns | grep web-server
+                then
+                    kubectl delete ns web-server
+                fi
+
+                kubectl create ns web-server
+
+                if ls | grep .php
+                then
+                    kubectl create -f Deployments/web.yml -n web-server
+
+                    kubectl create -f Deployments/service.yml -n web-server
+
+                    sleep 15
+
+                    podname=$(kubectl get pods -o=name -n web-server | grep phpserver | sed "s/^.\\{4\\}//")
+
+                    kubectl cp index.php $podname:/var/www/html -n web-server
+
+                    kubectl cp webdata $podname:/var/www/html -n web-server
+                else
+                    kubectl create -f Deployments/web-http.yml -n web-server
+
+                    kubectl create -f Deployments/service-http.yml -n web-server
+
+                    sleep 15
+
+                    podname=$(kubectl get pods -o=name -n web-server | grep httpserver | sed "s/^.\\{4\\}//")
+
+                    kubectl cp index.html $podname:/var/www/html -n web-server
+
+                    kubectl cp webdata $podname:/var/www/html -n web-server
+
+                fi 
+            ''')
+    }
+    triggers {
+        upstream('Task-6-Job-1', 'SUCCESS')
+    }
+}
+
+job("Task-6-Job-3") {
+    triggers {
+            upstream {
+                upstreamProjects('Task-6-Job-2')
+                threshold('SUCCESS')
+            }
+        }
+    steps {
+        shell('''
+if kubectl get pods -n web-server | grep phpserver
+then
+    status=$(curl -s -LI -w "%{http_code}" -o /dev/null 192.168.99.101:32323)
+    
+    if [ $status -eq 200 ];
+    then
+        echo "Website working"
+        exit 1
+    else
+        exit 0
+    fi
+else
+    status=$(curl -s -LI -w "%{http_code}" -o /dev/null 192.168.99.101:32324)
+
+    if [ $status -eq 200 ];
+    then
+        echo "Website working"
+        exit 1
+    else
+        exit 0
+    fi
+fi
+''')
+}
+}
+job("Task-6-Job-4"){
+    authenticationToken('mail')
+    publishers {
+        mailer("arunsumit878@gmail.com", false, false)
+    }
+    triggers {
+        upstream('Task-6-Job-3', 'SUCCESS')
+   }
+}
