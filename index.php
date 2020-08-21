fcaefaa666943f583c7aeeb81f5626bb1004defb
Arun Kumar <arunsumit878@gmail.com>
2020-08-21 13:31:55 +0530

    t10

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    priefnt("<h1>Hello World !!!</h1>");
+    printt("<h1>Hello World !!!</h1>");
     print("<h2>Image</h2>");
     print("<img src='webdata/im.jpg' width=600 height=600>");
 
