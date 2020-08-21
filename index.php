acb1502aaea1c4aae7120ea5b3c1bf1a876fec70
Arun Kumar <arunsumit878@gmail.com>
2020-08-21 12:55:45 +0530

    T5

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    print("<h1>Hello World !!!</h1>");
+    priefnt("<h1>Hello World !!!</h1>");
     print("<h2>Image</h2>");
     print("<img src='webdata/im.jpg' width=600 height=600>");
 
