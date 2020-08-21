06f9d2c0a8ebe4b75dceba2d50d7699b4118f1af
Arun Kumar <arunsumit878@gmail.com>
2020-08-21 13:09:26 +0530

    t7

--- myt6.groovy
+++ myt6.groovy
@@ -82,8 +82,8 @@ job("Task-6-Job-4"){
     post {
     always {
         emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
+    }   
     }
-}
     triggers {
         upstream('Task-6-Job-3', 'SUCCESS')
    }
